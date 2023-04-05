c16b34396705c1e42ba4640bb22d84ef6f3d0e70
ninjapreto52 <ninjapreto53@gmail.com>
2023-04-05 12:15:58 -0300

    diego

---
+++ tabuada.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+    <style>
+    .verde{
+        background-color:green;
+    }
+    .amarelo{
+        background-color:yellow;
+    }
+    </style>
+
+</head>
+<body>
+    <?php
+        $valor = $_POST["valor"];
+        $nome = $_POST["nome"];
+        echo($nome);
+        for($x=1;$x<=10;$x++)
+        {
+            if($x%2 == 0)
+            {           
+            echo("<div class='verde'>");
+            } 
+            else
+            {
+            echo("<div class='amarelo'>");
+            }
+
+            echo($valor * $x);
+            echo("<br>");
+        }
+    ?>
+
+</body>
+</html>
